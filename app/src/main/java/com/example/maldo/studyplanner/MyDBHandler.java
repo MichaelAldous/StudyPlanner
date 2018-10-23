df26868a2e5a4d5db9d9c99a1beda4be1128c337
Showan Simkhada <showansimkhada@hotmail.com>
2018-10-24 06:55:55 +1300

    last update

--- app/src/main/java/com/example/maldo/studyplanner/MyDBHandler.java
+++ app/src/main/java/com/example/maldo/studyplanner/MyDBHandler.java
@@ -16,14 +16,16 @@ public class MyDBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db)
     {
         // creating the pathways table
-        final String SQL_CREATE_PATHWAYS_TABLE = "CREATE TABLE " + pathwaysEntry.TABLE_PATHWAYS + "( " +
-                pathwaysEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        final String SQL_CREATE_PATHWAYS_TABLE = "CREATE TABLE " +
+                pathwaysEntry.TABLE_PATHWAYS + "( " +
+                pathwaysEntry.COLUMN_PATH_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 pathwaysEntry.COLUMN_PATH_NAME+ " TEXT NOT NULL " + ");";
         db.execSQL(SQL_CREATE_PATHWAYS_TABLE);
 
         // creating the modules table
-        final String SQL_CREATE_MODULES_TABLE = "CREATE TABLE " + modulesEntry.TABLE_MODULES + "( " +
-                modulesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        final String SQL_CREATE_MODULES_TABLE = "CREATE TABLE " +
+                modulesEntry.TABLE_MODULES + "( " +
+                modulesEntry.COLUMN_MOD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 modulesEntry.COLUMN_MOD_NAME+ " TEXT NOT NULL, " +
                 modulesEntry.COLUMN_MOD_PREREQ + " TEXT NOT NULL, " +
                 modulesEntry.COLUMN_MP_SEMESTER + " TEXT NOT NULL, " +
@@ -31,21 +33,24 @@ public class MyDBHandler extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_MODULES_TABLE);
 
         // creating the mod path table
-        final String SQL_CREATE_MODPATH_TABLE = "CREATE TABLE " + modPathsEntry.TABLE_MOD_PATH + "( " +
+        final String SQL_CREATE_MODPATH_TABLE = "CREATE TABLE " +
+                modPathsEntry.TABLE_MOD_PATH + "( " +
                 modPathsEntry.COLUMN_MP_MOD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 modPathsEntry.COLUMN_MP_PATH_ID + " INTEGER PRIMARY KEY AUTOINCREMENT "+ ");";
         db.execSQL(SQL_CREATE_MODPATH_TABLE);
 
         // creating the student table
-        final String SQL_CREATE_STUDENT_TABLE = "CREATE TABLE " + studentEntry.TABLE_STUDENTS + "( " +
-                studentEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        final String SQL_CREATE_STUDENT_TABLE = "CREATE TABLE " +
+                studentEntry.TABLE_STUDENTS + "( " +
+                studentEntry.COLUMN_STUD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 studentEntry.COLUMN_STUD_FNAME+ " TEXT NOT NULL, " +
                 studentEntry.COLUMN_STUD_LNAME+ " TEXT NOT NULL, " +
                 studentEntry.COLUMN_STUD_EMAIL+ " TEXT NOT NULL " + ");";
         db.execSQL(SQL_CREATE_STUDENT_TABLE);
 
         // creating the student module table
-        final String SQL_CREATE_STUDENTMODULE_TABLE = "CREATE TABLE " + studentModuleEntry.TABLE_STUD_MOD + "( " +
+        final String SQL_CREATE_STUDENTMODULE_TABLE = "CREATE TABLE " +
+                studentModuleEntry.TABLE_STUD_MOD + "( " +
                 studentModuleEntry.COLUMN_SM_STUD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 studentModuleEntry.COLUMN__SM_MOD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 studentModuleEntry.COLUMN__SM_STATUS + " TEXT NOT NULL "  + ");";
