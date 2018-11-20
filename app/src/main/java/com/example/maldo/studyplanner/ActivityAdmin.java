dce3596107eb898868af5b9666ff7a6f610d3089
Michael <maldous99@gmail.com>
2018-11-20 23:35:10 +1300

    Admin password fixed
    Added admin password stuff

--- app/src/main/java/com/example/maldo/studyplanner/ActivityAdmin.java
+++ app/src/main/java/com/example/maldo/studyplanner/ActivityAdmin.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityAdmin extends AppCompatActivity {
     @Override
@@ -12,16 +14,22 @@ public class ActivityAdmin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        final String password = "WinITDMP01";
 
+        final EditText pwInput = (EditText) findViewById(R.id.passwordInput);
         Button loginButton = findViewById(R.id.buttonLogin);
 
         //onClick listener for cake button, passes cake recipes to new intent
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ActivityAdmin.this, ActivityEditor.class);
-                startActivity(intent);
+                if(pwInput.getText().toString().equals(password)) {
+                    Intent intent = new Intent(ActivityAdmin.this, ActivityEditor.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Invalid Password",Toast.LENGTH_LONG).show();
             }
+         }
         });
     }
 }
